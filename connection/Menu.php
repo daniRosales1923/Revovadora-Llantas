d95ec45d96f9f77a3c1509885692f75efc9db588
Daniela Rosales <danielarosales139@gmail.com>
2019-05-12 18:57:29 -0500

    Avances de Modificacion en menu
    Avance del 50%

--- connection/Menu.php
+++ connection/Menu.php
@@ -1,30 +1,34 @@
 <?php
-    header('Content-Type: text/html; charset=UTF-8');
-    session_start();
-    if (isset($_SESSION['nombre'])){
-        $cliente = $_SESSION['nombre'];
-    }else{
- 		header('Location: LogIn.php');
-     	die() ;
-    }
+    // header('Content-Type: text/html; charset=UTF-8');
+    // session_start();
+    // if (isset($_SESSION['nombre'])){
+    //     $cliente = $_SESSION['nombre'];
+    // }else{
+ 	// 	header('Location: LogIn.php');
+    //  	die() ;
+    // }
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
     <head>
-      
+    <meta charset=“UTF-8”>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href = "../assets/css/main.css">
     </head>
 
     <body>
-        <div id="sesion_cliente">
+        <!-- <div id="sesion_cliente">
             <?php 
-            if(isset($_SESSION['nombre'])){
-                echo "<p> Bienvenido ".$cliente."&nbsp;&nbsp;";
-                echo "<a href='LogIn.php?salir=1'>Salir</a></p>";
-                if(isset($_REQUEST["salir"])){
-                    unset($_SESSION["cliente"]);
-                }
-            }
+            // if(isset($_SESSION['nombre'])){
+            //     echo "<p> Bienvenido ".$cliente."&nbsp;&nbsp;";
+            //     echo "<a href='LogIn.php?salir=1'>Salir</a></p>";
+            //     if(isset($_REQUEST["salir"])){
+            //         unset($_SESSION["cliente"]);
+            //     }
+            // }
             ?>
         </div>
 
@@ -37,8 +41,44 @@
             </ul>
 
             <div id="cuerpo_body">
-                <!-- Aqui va todo el código del cuerpo... -->
+                 Aqui va todo el código del cuerpo... 
             </div>
-        </div>
+        </div> -->
+
+        <section class="main">
+            <div class="section_logo">
+                <div id="logo">
+					<img src="../assets/img/logo2.png" width="195" height="195">
+				</div>
+            </div>
+
+            <div class="section_content">
+                <header class="section_content-nav">
+                    <div class="title">
+                        <h2 class="item_title">Renovadora de llantas</h2>
+                    </div>
+                    <nav class="navbar">
+                        <ul>
+                            <li><a href="">INICIO</a></li>
+                            <li><a href="">SERVICIOS</a></li>
+                            <li><a href="">CLIENTES</a></li>
+                            <li><a href="">CONTACTO</a></li>
+                            <li><a href="">AYUDA</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <div class="area_trabajo">
+                    Esta es un area de trabajo
+                </div>
+
+            </div>
+            
+        </section>
+		<footer class="footer">
+				<p class="footer-text">
+                <i class="fas fa-copyright"></i> Todos los derechos reservados - Instituto Tecnologico de Orizaba. <br>
+					Diseñado por alumnos del plantel.
+				</p>
+		</footer>
     </body>
 </html>
