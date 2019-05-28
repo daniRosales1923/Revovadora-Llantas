8016dcf0623eb217497f13154de76e8c2094a00b
Jonathan Castillo <castillo2363@gmail.com>
2019-05-28 17:39:19 -0500

    cambio texto luegar

--- connection/Entradas.php
+++ connection/Entradas.php
@@ -251,10 +251,6 @@
 			echo '<form id="LLantas">
 			<div class="entry-form__header">
 				<div class="aling__input">
-					<label id= "lblMatricula" name = "lblFolio" >Numero de serie</label>
-					<input class="form-llanta" type="text" name="txtNumSerie">
-				</div>
-				<div class="aling__input">
 					<label id= "lblMarca" name = "lblFolio" >Marca</label>
 					'. ComboMarcas() .'
 				</div>
@@ -265,6 +261,10 @@
 				<div class="aling__input">
 					<label id= "Trabajo" name= "lblUsuario"> Trabajo </label>
 					'. ComboTrabajo($_SESSION["Modelo"]).'
+				</div>
+				<div class="aling__input">
+					<label id= "lblMatricula" name = "lblFolio" >Numero de serie</label>
+					<input class="form-llanta" type="text" name="txtNumSerie">
 				</div>
 				<input class="buttons-save" type="submit" name="GuardaLllanta" value="Agregar" >
 			</div>
